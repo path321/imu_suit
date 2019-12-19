03502128f3dbd795d597b48e55a1d13a1eae5f90
path321 <petath321@gmail.com>
2019-12-19 13:13:05 +0200

    added LP,bash,az functions

--- various_func.py
+++ various_func.py
@@ -2,19 +2,39 @@ import numpy as np
 from subprocess import run,PIPE
 
 
-def linearMap(value,newMax,newMin,oldMax,oldMin):  #Map values to new limits
+def linearMap(value,newMax,newMin,oldMax,oldMin):
+        "Map values to new limits"
         return (value-oldMin)*(newMax-newMin)/(oldMax-oldMin)+newMin
 
 
-def appendData(myList,val): #Update the graph line with new data
+def appendData(myList,val):
+        '''perfrom FIFO insertion in a list
+           alternative : deque.append() deque.pop()'''
         lenList = len(myList)
         myList=np.append(myList,val)
         myList=myList[-lenList:]
         return myList
+        
+def lowPass(now,prev,a):
+        "1st order low pass filter in 1D"
+        return (a*now + (1-a)*prev)
 
+def lowPass2(now,prev,prev2):
+        "2nd order low pass filter in 1D"
+        return (0.7*now + 0.2*prev+ 0.1*prev2)
 
-def runBash(inpt): #Run bash commands in terminal , get output and error
-        # !! Security hazard, User beware of the input command !! 
+def checkAz(inpt):
+        "Limit input in [180,-180)"
+        if(inpt > 180):
+                inpt -= 360
+        elif(inpt <= -180):
+                inpt += 360
+        return inpt
+
+def runBash(inpt):
+        '''Run bash commands in terminal , get output and error
+        !! Security hazard, User beware of the input command !!
+        '''
         bashCommand = inpt 
         process = run(bashCommand,shell=True, stderr=PIPE, stdout=PIPE, universal_newlines=True)
         output = (process.stdout).strip()
